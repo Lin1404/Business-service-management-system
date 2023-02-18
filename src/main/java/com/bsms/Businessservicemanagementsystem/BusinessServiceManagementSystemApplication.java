7a62db6d60770434677eef0e2fce81dbaec1b264
weixiang237 <87122779+weixiang237@users.noreply.github.com>
2023-02-17 22:06:16 -0500

    Backend dev (#21)
    * Update README.md
    
    * initialization of springboot
    
    * Maven dependencies for DynamoDB added
    
    * Updated Readme
    
    * moved files up one level
    
    * .gitignore file added
    
    ---------
    
    Co-authored-by: Lin1404 <78375725+Lin1404@users.noreply.github.com>

---
+++ src/main/java/com/bsms/Businessservicemanagementsystem/BusinessServiceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bsms.Businessservicemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessServiceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessServiceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsms/Businessservicemanagementsystem/BusinessServiceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsms.Businessservicemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessServiceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
